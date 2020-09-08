5eff75e1863f3359bd537d16601af2ff45a667c8
Michael Ibragimchayev <michael.ibragimchayev@gmail.com>
2020-09-07 22:14:26 -0400

    build site into docs directory

---
+++ docs/precache-manifest.0d4d27fdf6dcd3feba665869a6748088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111993c7bd5eb75ad9c3045e0f9f71f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae37aa002371bae968e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae37aa002371bae968e",
+    "url": "/static/js/main.a570ab73.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
